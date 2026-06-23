7b11b306666fd94308ae47b9b8fe041cab1ccdf3
Harsha <harshabm16@gmail.com>
2026-06-23 20:24:52 +0530

    changed

--- backend/routes.js
+++ backend/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+//const path = require('path');
 const pool = require('./db');
 const { login, requireAdmin } = require('./auth');
 const supabase = require('./supabase');
